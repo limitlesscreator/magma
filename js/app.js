ba4592a27f6869238642355b786bec2e09af760a
limitlesscreator <0428vlad0428@mail.ru>
2024-06-11 21:47:27 +0300

    next

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
+document.querySelector('.closeBtn').addEventListener('click',() => {
+  document.querySelector('.modal-1').style.opacity = 0
+  document.querySelector('.modal-1').style.zIndex = -100
 
+  document.querySelector('.modalContactForTwo').style.opacity = 0
+  document.querySelector('.modalContactForTwo').style.zIndex = -100
+})
+
+
+document.querySelector('.bookBtn').addEventListener('click',() => {
+  document.querySelector('.modal-1').style.opacity = 1
+  document.querySelector('.modal-1').style.zIndex = 100
+
+  document.querySelector('.modalContactForTwo').style.opacity = 1
+  document.querySelector('.modalContactForTwo').style.zIndex = 100
+})
+
+document.querySelector('.bookBtn2').addEventListener('click',() => {
+  document.querySelector('.modal-1').style.opacity = 1
+  document.querySelector('.modal-1').style.zIndex = 100
+
+  document.querySelector('.modalContactForTwo').style.opacity = 1
+  document.querySelector('.modalContactForTwo').style.zIndex = 100
+})
+
+document.querySelector('.bookBtn3').addEventListener('click',() => {
+  document.querySelector('.modal-1').style.opacity = 1
+  document.querySelector('.modal-1').style.zIndex = 100
+
+  document.querySelector('.modalContactForTwo').style.opacity = 1
+  document.querySelector('.modalContactForTwo').style.zIndex = 100
+})
